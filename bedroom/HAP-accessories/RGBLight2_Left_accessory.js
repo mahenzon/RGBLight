4c70e75e11a4c5a476bfb12e21bb816a4842acf6
Suren Khorenyan <surenkhorenyan@gmail.com>
2018-11-05 18:03:06 +0300

    Create 4 separate accessories for one bedroom (4 walls)

---
+++ bedroom/HAP-accessories/RGBLight2_Left_accessory.js
@@ -0,0 +1,22 @@
+var newRGBLight = require('./RGBLight_helper').newRGBLight;
+
+// ********************* SETUP YOUR ACCESSORY
+
+// This value changes for every independent controller
+var mqttLightTopic = '/home/RGBLight02';  // MQTT topic to publish updates
+
+// This value changes for each side of each controller
+const currentSide = 'l';  // l for left, r for right
+
+// These two values have to be unique for each accessory!
+const macAddr = 'A3:D0:81:F4:BA:9A';
+const accessoryName = '2 Left';
+
+// ************* END SETUP
+
+
+// function(name, mac, currentSide, mqttLightTopic)
+var RGBLight = newRGBLight(accessoryName, macAddr, currentSide, mqttLightTopic);
+
+// export HomeKit Accessory
+exports.accessory = RGBLight.lightAccessory;
